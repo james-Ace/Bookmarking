6e7fde10b967293c8c81887de8dcdc5b5b59e1ec
james <james@bromel.cn>
2023-05-14 22:13:33 +0800

    初始化环境

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="common-header">Header</el-header>
+      <el-main>
+        <RouterView />
+      </el-main>
+    </el-container>
+  </div>
+
+  
+</template>
+
+<style scoped>
+.common-header{
+  height: 80px;
+  background: #414141;
+}
+</style>
